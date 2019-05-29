3ebde76cbdc4516ff17a8c72a5e1f47f4c77766a
shadeying <shadeying@gmail.com>
2019-05-29 18:41:17 +0000

    Withdrawals validated

--- index.js
+++ index.js
@@ -30,10 +30,22 @@ class Transaction {
   }
 
   commit() {
-    // Keep track of the time of the transaction
-    this.time = new Date();
-    // Add the transaction to the account
-    this.account.addTransaction(this);
+    if(this.isAllowed){
+      // Keep track of the time of the transaction
+      this.time = new Date();
+      // Add the transaction to the account
+      this.account.addTransaction(this);
+    }else{
+      console.log('Error');
+    }
+
+  }
+
+  get isAllowed() {
+    if(this.value + this.account.balance < 0) {
+      return false;
+    }
+    return true;
   }
 
 }
@@ -69,5 +81,8 @@ t1.commit();
 
 const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
+
+const t3 = new Withdrawal(500.00, myAccount);
+t3.commit();
 
 console.log('Ending Balance:', myAccount.balance);
